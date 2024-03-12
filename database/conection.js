1d883fd378dc0cf86c4d70ec5fa9d427cfc86c28
Cristian Andres Abondano Vasquez <caabondano@misena.edu.co>
2024-03-12 11:29:38 -0500

    correccion de coneccion

--- database/conection.js
+++ database/conection.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 // URL de conexión a tu base de datos (cambia según tus credenciales y configuración)
 const dbUrl = "mongodb+srv://abondano930719:Novo12345@cluster9307.lknvmo6.mongodb.net/mi_blog"; // Cambia "mi_blog" al nombre de tu base de datos
 
+// Opciones de conexión de MongoDB
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 // Conexión a MongoDB
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-   
-});
+mongoose.connect(dbUrl, dbOptions)
+  .then(() => console.log("Conexión exitosa a la base de datos"))
+  .catch(error => console.error("Error de conexión:", error));
 
 // Manejo de eventos de conexión
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Error de conexión:"));
-db.once("open", () => {
-    console.log("Conexión exitosa a la base de datos");
-});
 
 // Exporta la conexión para usarla en otros archivos
 module.exports = db;
